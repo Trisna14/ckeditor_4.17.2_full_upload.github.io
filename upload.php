55daeab5783eb4ad8d74a31b3771c09b23860d41
emde5 <trisna.tkjtamsisjetis14@gmail.com>
2022-02-16 22:18:03 +0700

    1

---
+++ upload.php
@@ -0,0 +1,32 @@
+<?php 
+
+    function uploadFile () {
+        if (isset($_FILES['upload']['tmp_name'])) {
+            $file = $_FILES['upload']['tmp_name'];
+            $fileName = $_FILES['upload']['name'];
+            $fileNameArray = explode('.', $fileName);
+            $extension = end($fileNameArray);
+            $newName = rand() . '.' . $extension;
+            $allow = ['jpg','jpeg','png','JPG','JPEG'];
+    
+            if (in_array($extension, $allow)) {
+                move_uploaded_file($file, 'img/' . $newName);
+    
+                $functionNumber = $_GET['CKEditorFuncNum'];
+    
+                $url = 'img/' . $newName;
+    
+                // $message = '';
+    
+                echo 
+                "<script type='text/javascript'>
+                    // console.log('$functionNumber,$url');
+                    window.parent.CKEDITOR.tools.callFunction($functionNumber, '$url');
+                    // console.log(window.parent.CKEDITOR.tools.callFunction);
+                </script>";
+            }
+        }
+    }
+
+    uploadFile();
+?>
